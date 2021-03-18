bd0a751d0186322e2754183816fa4587e2d777a7
djaroszewicz <jaroszewicz.dawid.kamil@gmail.com>
2021-03-18 20:33:11 +0100

    #AddCarModel

---
+++ CarPartners/Areas/Home/Models/Db/Dealer/DealerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCars.Areas.Home.Models.Db.Dealer
+{
+    public class DealerModel
+    {
+    }
+}
---
+++ CarPartners/Areas/Home/Models/View/Car/CarView.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCars.Areas.Home.Models.View.Car
+{
+    public class CarView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int HorsePower { get; set; }
+        public string Color { get; set; }
+        public int NumberOfDoors { get; set; }
+        public int Seats { get; set; }
+        public string Mark { get; set; }
+        public double Capacity { get; set; }
+        public double Cost { get; set; }
+        public double Mileage { get; set; }
+    }
+}
