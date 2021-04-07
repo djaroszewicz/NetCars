6051dc1b64d5520fa7180bdfcf7ad0c5efef281e
djaroszewicz <jaroszewicz.dawid.kamil@gmail.com>
2021-04-07 17:49:55 +0200

    Edycja CRUD samochodu

---
+++ CarPartners/Areas/Home/Models/View/Role/RoleView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCars.Areas.Home.Models.View.Role
+{
+    public class RoleView
+    {
+        [Required(ErrorMessage = "Nazwa roli jest wymagana!")]
+        public string RoleName { get; set; }
+    }
+}
---
+++ CarPartners/Infrastructure/Helpers/RoleHelpers.cs
@@ -0,0 +1,42 @@
+﻿using NetCars.Areas.Home.Models.Db.Role;
+using NetCars.Areas.Home.Models.View.Role;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCars.Infrastructure.Helpers
+{
+    public static class RoleHelpers
+    {
+        public static RoleModel ConvertToModel(RoleView result)
+        {
+
+            var roleModel = new RoleModel
+            {
+                RoleName = result.RoleName
+
+            };
+
+            return roleModel;
+        }
+
+        public static RoleView ConvertToView(RoleModel result)
+        {
+            var roleView = new RoleView
+            {
+                RoleName = result.RoleName
+            };
+
+            return roleView;
+        }
+
+
+        public static RoleModel MergeViewWithModel(RoleModel model, RoleView view)
+        {
+            model.RoleName = view.RoleName;
+
+            return model;
+        }
+    }
+}
